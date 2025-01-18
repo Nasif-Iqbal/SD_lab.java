9d834b41ec94d588b0eb980e03c27558418aabf5
NASIF IQBAL <150609244+Nasif-Iqbal@users.noreply.github.com>
2025-01-18 12:42:23 +0600

    Update Method_overloading.java

--- Method_overloading.java
+++ Method_overloading.java
@@ -27,3 +27,55 @@ public class Sum {
         System.out.println(s.sum(10.5, 20.5));
     }
 }
+
+// *****************single inheritance**************
+// Java Program to illustrate Inheritance (concise)
+
+import java.io.*;
+
+// Base or Super Class
+class Employee {
+    int salary = 60000;
+}
+
+// Inherited or Sub Class
+class Engineer extends Employee {
+    int benefits = 10000;
+}
+
+// Driver Class
+class Gfg {
+    public static void main(String args[])
+    {
+        Engineer E1 = new Engineer();
+        System.out.println("Salary : " + E1.salary
+                           + "\nBenefits : " + E1.benefits);
+    }
+}
+******************Multiple inheritance************
+    // Java Doesn't support Multiple Inheritance
+import java.io.*;
+
+// First Parent Class 
+class Parent1 {
+    void fun() { System.out.println("Parent1"); }
+}
+
+// Second Parent Class
+class Parent2 {
+    void fun() { System.out.println("Parent2"); }
+}
+
+// Inheriting Properties of
+// Parent1 and Parent2
+class Test extends Parent1, Parent2 
+{
+      // main method
+    public static void main(String args[])
+    {
+          // Creating instance of Test
+        Test t = new Test();
+
+        t.fun();
+    }
+}
